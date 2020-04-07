1b25d66d4aeb43d497a2cb675a17e99081234fa1
Dloft <derek.lofton1@gmail.com>
2020-04-06 23:13:05 -0400

    logic

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ function Post() {
 
     console.log(UserInput)
     // console.log("Button response Working")
-    document.getElementById("chat-script").innerHTML(UserInput);
+
+    if (UserInput == "shit") {
+        UserInput = " poop "
+    } else if (UserInput == "fuck") {
+        UserInput = " darn "
+    } else if (UserInput == "bitch") {
+        UserInput = " girl dog "
+    } else {
+        UserInput = document.getElementById("chat-input").value;
+    }
+    
+    // document.getElementById("bubble").innerHTML = (UserInput);
+    document.getElementById("bubble").append(UserInput);
+    // $("bubble").html(UserInput);
+    
 }
